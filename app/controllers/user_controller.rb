e862731bc75c4764a4add3268ed394209b0e3264
cmho <carlymho@gmail.com>
2016-01-18 19:47:23 -0600

    #10 Create user settings view and implement backend processing

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,7 +12,13 @@ class UserController < ApplicationController
 	end
 
 	def update
-
+		@user = User.find(session[:user])
+		if @user.update_attributes!(params[:user])
+			flash[:success] = "You've updated your settings successfully."
+		else
+			flash[:error] = "There was an error updating your settings."
+		end
+		redirect_to edit_user_path(@user)
 	end
 
 	def destroy
@@ -22,6 +28,6 @@ class UserController < ApplicationController
 	private
 
 	def user_params
-
+		params.require(:user).permit(:display_name, :email, :username, :encrypted_password, :current_sign_in_ip, :last_sign_in_ip, :current_sign_in_at, :last_sign_in_at, :reset_password_sent_at, :remember_created_at, :reset_password_token)
 	end
 end
