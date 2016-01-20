5bc633f8e6675e3f86b5ee2429d27423e0daad9c
Carly Ho <carlymho@gmail.com>
2016-01-20 14:24:11 -0600

    #30 Implement route for stats validation on characters in creation process

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -138,6 +138,19 @@ class CharactersController < ApplicationController
 		render json: {name: @power.name, description: @power.description, requires_specification: @power.requires_specification, specification_name: @power.specification_name, tier: @power.tier, select_multiple: @power.select_multiple, select_max: @power.select_max}
 	end
 
+	def validate_stats
+		@character = Character.find(params[:id])
+		tier1 = @character.powers.where({tier: 1}).count * 3
+		tier2 = @character.powers.where({tier: 2}).count * 5
+		backgrounds = (@character.item + @character.money + @character.allies - 2, 0).min
+		knacks = (@character.knacks.length - 3, 0).min
+		respond_to do |format|
+			format.json {
+				render json: {build_points: tier1 + tier2 + backgrounds + knacks, knacks_ok: knacks >= 3, backgrounds_ok: backgrounds >= 2}
+			}
+		end
+	end
+
 	private
 
 	def character_params
