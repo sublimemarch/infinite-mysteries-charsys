7fa76f8ab88424f2de53ce89ebe428d1dba305e7
Carly Ho <carlymho@gmail.com>
2016-01-21 13:38:15 -0600

    Fix character sheet view

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160119163851) do
-=======
 ActiveRecord::Schema.define(version: 20160120211129) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
