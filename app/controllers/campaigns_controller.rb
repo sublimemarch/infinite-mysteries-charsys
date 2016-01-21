27484b7832e80aff636fdeb5b80ae65ef77e57db
Carly Ho <carlymho@gmail.com>
2016-01-21 10:31:46 -0600

    Fill out methods on campaign controller, add model validations, add token input JS and campaign forms

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,32 +2,50 @@ class CampaignsController < ApplicationController
 	before_action :authenticate_user!
 	
 	def index
-
+		@campaigns = Campaign.where({user: current_user})
 	end
 	
 	def show
-
+		@campaign = Campaign.find(params[:id])
 	end
 
 	def new
-
+		@campaign = Campaign.new
 	end
 
 	def create
-
+		@campaign = Campaign.new(campaign_params)
+		if @campaign.save!
+			flash[:success] = "Your new campaign was successfully saved."
+			redirect_to campaign_path(@campaign)
+		else
+			flash[:error] = "There was an error creating your campaign."
+			redirect_to new_campaign_path
+		end
 	end
 
 	def edit
-
+		@campaign = Campaign.find(params[:id])
 	end
 
 	def update
+		@campaign = Campaign.find(params[:campaign][:id])
+		if @campaign.update_attributes!(campaign_params)
+			flash[:success] = "The changes to your campaign were successfully saved."
+			redirect_to campaign_path(@campaign)
+		else
+			flash[:error] = "There was an error saving the changes to your campaign."
+			redirect_to edit_campaign_path(@campaign)
+		end
+	end
 
+	def find_storyteller
+		@storytellers = User.where("email LIKE ?", params[:q])
 	end
 
 	private
 
 	def campaign_params
-
+		params.require(:campaign).permit(:name, :user_administers_campaign, :build_points)
 	end
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,8 @@
 class Campaign < ActiveRecord::Base
+	validates :name, presence: true
+	validates :build_points, presence: true
 	has_many :user_administers_campaigns
 	has_many :users, through: :user_administers_campaigns
+
+	accepts_nested_attributes_for :user_administers_campaigns
 end
