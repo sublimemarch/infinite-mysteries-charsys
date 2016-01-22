c13a37b8d2a867de5ab1117ee2101d23f110959b
Carly Ho <carlymho@gmail.com>
2016-01-22 09:55:42 -0600

    Fix missing files on character model; implement js for flaws and knacks

---
+++ db/migrate/20160122154348_add_campaign_id_field_to_character.rb
@@ -0,0 +1,5 @@
+class AddCampaignIdFieldToCharacter < ActiveRecord::Migration
+  def change
+  	add_column :characters, :campaign_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121194239) do
+ActiveRecord::Schema.define(version: 20160122154437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,8 +53,11 @@ ActiveRecord::Schema.define(version: 20160121194239) do
     t.text     "money_description"
     t.text     "allies_description"
     t.integer  "stress_max"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "status",             default: 0
+    t.integer  "campaign_id"
+    t.integer  "broad_type_id"
   end
 
   create_table "flaws", force: :cascade do |t|
