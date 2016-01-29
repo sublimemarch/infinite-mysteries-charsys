d518dae568e8603e036712c5875335f0c009a101
Stephanie Marx <stephanie.n.marx@gmail.com>
2016-01-28 22:01:54 -0600

    Pass necessary variables to index partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      
+      @campaigns = current_user.campaigns
+      @characters = current_user.characters
     else
       redirect_to login_path
     end
