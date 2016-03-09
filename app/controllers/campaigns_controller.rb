11e9eb043e1360f4c12b01c0b04524fc67540036
Carly Ho <cho@cliquestudios.com>
2016-03-09 10:46:20 -0600

    Fix delete confirmations

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -8,7 +8,7 @@ class CampaignsController < ApplicationController
 	def show
 		@campaign = Campaign.find(params[:id])
 		unless @campaign.users.include?(current_user)
-			redirect_to campaigns_path
+			redirect_to root_path
 		end
 	end
 
@@ -75,7 +75,7 @@ class CampaignsController < ApplicationController
 	def destroy
 		@campaign = Campaign.find(params[:id])
 		@campaign.delete
-		redirect_to campaigns_path
+		redirect_to root_path
 	end
 
 	def find_storyteller
